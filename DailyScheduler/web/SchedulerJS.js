09ea0538b9279b7c064d9a3f48e33c2ab215b70c
Rahul Rana <35415528+Misto1997@users.noreply.github.com>
2018-10-09 11:14:42 +0530

    added

---
+++ DailyScheduler/web/SchedulerJS.js
@@ -0,0 +1,42 @@
+function addItems()
+ {
+    var input=document.getElementById('input');
+    var item=input.value;
+    var ul=document.getElementById('list');
+    var textnode=document.createTextNode(item);
+    
+    if(item==='')
+      {
+         return false;
+      }
+    else
+      {
+         //created li
+         var li=document.createElement('li');
+        
+         //created checkbox
+         var checkbox=document.createElement('input');
+         checkbox.type='checkbox';
+         checkbox.setAttribute('id','check');
+        
+         //created label
+         var label=document.createElement('label');
+         label.setAttribute('for','item');
+        
+         // adding these to webpage      
+         li.appendChild(checkbox);
+         label.appendChild(textnode);
+         li.appendChild(label);
+         ul.insertBefore(li,ul.childNodes[0]);
+         input.value='';
+         setTimeout(()=>{li.className='visual';},2)
+        
+      }
+ }
+ 
+function removeItems()
+{
+  var ul=list;
+  ul.innerHTML='';
+} 
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DailyScheduler
+Description:-
+ ->It's a todo application for your daily scheduler.
+ ->you can add schedule anytime and reset it whenever you want.
+ 
+Language used:-
+ HTML,CSS,JavaScript
